1975ab7c6d37131f9e0a4257ba70b997cd170c5f
Кирилл Майданников <mk@intervolga.ru>
2024-09-18 14:20:48 +0300

    Убрал ненужную библиотеку из сборки

--- src/test/java/org/opensearch/rest/action/StatisticsPluginIT.java
+++ src/test/java/org/opensearch/rest/action/StatisticsPluginIT.java
@@ -1,14 +1,14 @@
 package org.opensearch.rest.action;
 
 import com.carrotsearch.randomizedtesting.annotations.ThreadLeakScope;
-import org.apache.hc.core5.http.ParseException;
-import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.opensearch.client.Request;
 import org.opensearch.client.Response;
 import org.opensearch.plugins.Plugin;
 import org.opensearch.test.OpenSearchIntegTestCase;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.Collection;
 import java.util.Collections;
@@ -24,9 +24,19 @@ public class StatisticsPluginIT extends OpenSearchIntegTestCase {
         return Collections.singletonList(StatisticsPlugin.class);
     }
 
-    public void testPluginInstalled() throws IOException, ParseException {
+    public void testPluginInstalled() throws IOException {
         Response response = getRestClient().performRequest(new Request("GET", "/_cat/plugins"));
-        String body = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
+
+        String body;
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), StandardCharsets.UTF_8))) {
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+            }
+            body = sb.toString();
+        }
 
         logger.info("response body: {}", body);
         assertThat(body, containsString("statistics"));
